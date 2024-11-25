a81efcbcd74dec53cf973ef83714a453f929ff1e
ckamilokoo <camilo.camposog@gmail.com>
2024-11-25 01:38:48 -0300

    link para devolverse a la pantalla de inicio

--- src/modules/admin/layouts/AdminLayout.vue
+++ src/modules/admin/layouts/AdminLayout.vue
@@ -19,9 +19,9 @@
           />
         </svg>
       </a>
-      <a
+      <RouterLink
+        to="/"
         class="flex items-center justify-center flex-shrink-0 w-10 h-10 mt-4 rounded hover:bg-gray-300"
-        href="#"
       >
         <svg
           class="w-5 h-5"
@@ -37,7 +37,7 @@
             d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
           />
         </svg>
-      </a>
+      </RouterLink>
       <a
         class="flex items-center justify-center flex-shrink-0 w-10 h-10 mt-4 rounded hover:bg-gray-300"
         href="#"
